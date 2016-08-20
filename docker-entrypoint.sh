08cf3eee5443cbc3ae10407770fbde4841fa4b0a
kecorbin <kecorbin@cisco.com>
2016-08-20 14:31:35 -0500

    migrations hell

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Make Migrations"
+
 # all app migrations should go on a single line
 # http://stackoverflow.com/questions/36153748/django-1-9-makemigrations-no-changes-detected
-
-python manage.py makemigrations reddit
+python manage.py makemigrations reddit users comments submissions
 
 
 # Apply database migrations
