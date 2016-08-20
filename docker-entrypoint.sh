f64337bf6f24fe74eea9ea740b85789e74980949
kecorbin <kecorbin@cisco.com>
2016-08-20 11:55:22 -0500

    modify docker entrypoint to reference specific app migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,14 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Make Migrations"
-python manage.py makemigrations
+python manage.py makemigrations reddit
+python manage.py makemigrations django_reddit
+
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python manage.py migrate reddit
+python manage.py migrate django_reddit
 
 # Start server
 echo "Starting server"
